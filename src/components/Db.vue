5197ad7691cfa4b3523b73e12b8467fb882f5bdd
veto <veto@body.body-piercing.com>
2026-03-02 18:01:57 +0700

    up: index.html src/components/Db.vue

--- src/components/Db.vue
+++ src/components/Db.vue
@@ -1,71 +1,64 @@
 <script setup>
 import Button from "primevue/button";
-import InputText from 'primevue/inputtext';
-import IftaLabel from 'primevue/iftalabel';
-import { ref,onMounted,inject } from "vue";
-  const items = ref([
-  ]);
+import InputText from "primevue/inputtext";
+import IftaLabel from "primevue/iftalabel";
+import { ref, onMounted, inject } from "vue";
+const items = ref([]);
 
 //const log = inject('log');
-const log = inject('logService');
-const worker = inject('Worker');
-const Workers  = inject('Workers');
+const log = inject("logService");
+const worker = inject("Worker");
+const Workers = inject("Workers");
 
-onMounted(() => {
-});
+onMounted(() => {});
 
 const init_sqlite3 = async () => {
   log.info("...init_sqlite3");
-  let msg = await Workers.post_message(worker,"test","my message");
+  let msg = await Workers.post_message(worker, "test", "my message");
   log.info(msg);
 };
 
-const name = ref('');
-const code = ref('');
-const qty = ref('');
+const name = ref("");
+const code = ref("");
+const qty = ref("");
 
 const insert_security = async () => {
   log.info("...insert_security");
   log.info("name: " + name.value);
   log.info("code: " + code.value);
-  log.info("qty: " + qty.value);    
+  log.info("qty: " + qty.value);
 
   //let msg = await Workers.post_message(worker,"test","my message");
   //log.info(msg);
 };
-
 </script>
 
-
-
 <template>
-<Button label="Init Sqlite3" @click="init_sqlite3" class="db_sqlite3" /><br>
+  <Button label="Init Sqlite3" @click="init_sqlite3" class="db_sqlite3" /><br />
 
-
-<IftaLabel>
+  <IftaLabel>
     <InputText id="name" v-model="name" />
     <label for="name">Name</label>
+  </IftaLabel>
 
-</IftaLabel>
-
-<IftaLabel>
+  <IftaLabel>
     <InputText id="code" v-model="code" />
-    <label for="code">Code</label>    
-</IftaLabel>
+    <label for="code">Code</label>
+  </IftaLabel>
 
-<IftaLabel>
+  <IftaLabel>
     <InputText id="qty" v-model="qty" />
-    <label for="qty">qty</label>    
-</IftaLabel>
-<Button label="Insert Security" @click="insert_security" class="insert_security" /><br>
-
+    <label for="qty">qty</label>
+  </IftaLabel>
+  <Button
+    label="Insert Security"
+    @click="insert_security"
+    class="insert_security"
+  /><br />
 </template>
 
 <style>
-  .a2d {
-   white-space: nowrap;
-  }
+.a2d {
+  white-space: nowrap;
+}
 </style>
-
-
-
