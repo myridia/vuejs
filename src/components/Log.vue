9914988dfda0880d6944d426787ffb26b4dcc7c9
veto <veto@body.body-piercing.com>
2026-03-03 09:01:38 +0700

    up: src/components/Log.vue src/components/Menubar.vue

--- src/components/Log.vue
+++ src/components/Log.vue
@@ -1,14 +1,24 @@
 <script setup>
-
+import Textarea from "primevue/textarea";
+import IftaLabel from "primevue/iftalabel";
+import { ref, onMounted, inject } from "vue";
+const logger = ref([]);
+logger.value = "xxxxx";
 </script>
 <template>
-  <textarea id="logger"></textarea>
+  <IftaLabel>
+    <Textarea
+      id="logger"
+      v-model="logger"
+      rows="10"
+      cols="100"
+      style="resize: none"
+    />
+    <label for="logger">Logger:</label>
+  </IftaLabel>
 </template>
 
 <style scoped>
 #logger {
-    width:99%;
-    height: 300px;
-    margin-left: 15px;
 }
 </style>
