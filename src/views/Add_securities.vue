83d065ffde3661f864dc109f3101f5b1a7e45bca
veto <veto@lab.local>
2026-03-08 16:52:42 +0700

    up: src/components/Add_Securities.vue src/views/Add_securities.vue src/views/Home.vue

--- src/views/Add_securities.vue
+++ src/views/Add_securities.vue
@@ -1,7 +1,7 @@
 <script setup>
 import Breadcrumb from "../components/Breadcrumb.vue";
 import Table from "../components/Table.vue";
-import Db from "../components/Db.vue";
+import Add_Securities from "../components/Add_Securities.vue";
 
 import { ref } from "vue";
 const items = ref([{ label: "Securities", route: "securities" }]);
@@ -9,7 +9,7 @@ const items = ref([{ label: "Securities", route: "securities" }]);
 
 <template>
   <Breadcrumb :items="items" />
-  <Db />
+  <Add_Securities />
 </template>
 
 <style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <script setup>
 import Breadcrumb from "../components/Breadcrumb.vue";
-import Db from "../components/Db.vue";
+
 import { ref, onMounted, inject } from "vue";
 const items = ref([]);
 </script>
